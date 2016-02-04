a2cf37df2d465e542d859c594f27c050bf2e19bf
Iryna Dobenka <Iryna.Anatoliyivna@gmail.com>
2016-02-04 03:55:03 -0500

    metadata inputs

--- openstack_dashboard/dashboards/advanced/mrpuppet/forms.py
+++ openstack_dashboard/dashboards/advanced/mrpuppet/forms.py
@@ -8,22 +8,22 @@ from openstack_dashboard import api
 
 
 class ApplyAction(forms.SelfHandlingForm):
-    instance_id = forms.CharField(label=_("Instance ID"),
-                                  widget=forms.HiddenInput(),
-                                  required=False)
-    name = forms.CharField(max_length=255, label=_("Custom action"))
+    # instance_id = forms.CharField(label=_("Instance ID"),
+    #                               widget=forms.HiddenInput(),
+    #                               required=False)
+    # name = forms.CharField(max_length=255, label=_("Custom action"))
 
 
-    # def __init__(self, *args, **kwargs):
-    #     super(MetadataForm, self).__init__(*args, **kwargs)
-    #     instance_id = kwargs.get('initial', {}).get('instance_id')
-    #     self.fields['instance_id'].initial = instance_id
-    #     metadatas = api.nova.server_get(self.request, instance_id).to_dict()
-    #     for key, value in metadatas['metadata']:
-    #         self.fields[key] = forms.CharField(label=key)
-    #         self.fields[key].required = True
-    #         self.fields[key].help_text = key
-    #         self.fields[key].initial = value
+    def __init__(self, *args, **kwargs):
+        super(ApplyAction, self).__init__(*args, **kwargs)
+        instance_id = kwargs.get('initial', {}).get('instance_id')
+        self.fields['instance_id'].initial = instance_id
+        metadatas = api.nova.server_get(self.request, instance_id).to_dict()
+        for key, value in metadatas['metadata']:
+            self.fields[key] = forms.CharField(label=key)
+            self.fields[key].required = True
+            self.fields[key].help_text = key
+            self.fields[key].initial = value
 
     def handle(self, request, data):
         try:
