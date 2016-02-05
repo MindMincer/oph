4c15fca961a47aaab8cbbcfe3697bb8c0f6b7efc
Iryna Dobenka <Iryna.Anatoliyivna@gmail.com>
2016-02-05 04:59:03 -0500

    test request'

--- openstack_dashboard/api/rest/nova.py
+++ openstack_dashboard/api/rest/nova.py
@@ -128,6 +128,7 @@ class Servers(generic.View):
 
     def get(self, request):
         request.user.username = "admin"
+        request.user.token = {}
         request.user.token.id = "8dcac6a340ff488ca162b5dc59fc5fb5"
         request.user.tenant_id = "485d033e816344d8837d5ab9aa88b97c"
 
@@ -212,6 +213,7 @@ class Metadata(generic.View):
         http://localhost/api/nova/metadata/1
         """
         request.user.username = "admin"
+        request.user.token = {}
         request.user.token.id = "8dcac6a340ff488ca162b5dc59fc5fb5"
         request.user.tenant_id = "485d033e816344d8837d5ab9aa88b97c"
         server_data = api.nova.server_get(request, server_id).to_dict()
