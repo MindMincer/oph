a0b97e8c21243a064bf53c32c2bdeaf7f5b5a639
Iryna Dobenka <Iryna.Anatoliyivna@gmail.com>
2016-02-08 11:02:26 -0500

    typos

--- openstack_dashboard/dashboards/advanced/mrpuppet/forms.py
+++ openstack_dashboard/dashboards/advanced/mrpuppet/forms.py
@@ -72,21 +72,21 @@ class EditENCMetadata(forms.SelfHandlingForm):
         classes = {"class12": {"Param11":"param 1 var for class 1", "Param12":"param 2 var for class 1", "Param13":"param 3 var for class 1", "Param14":"param 4 var for class 1"},
                         "class22": {"Param21":"param 1 var for class 2", "Param22":"param 2 var for class 2", "Param23":"param 3 var for class 2", "Param24":"param 4 var for class 2", "Param25":"param 5 var for class 2", "Param26":"param 6 var for class 2"},
                         "class32": {"Param31":"param 1 var for class 3", "Param32":"param 2 var for class 3"}}
-        return classes[class_name]
+        return classes["class12"]
 
     def __init__(self, request, *args, **kwargs):
         super(EditENCMetadata, self).__init__(request, *args, **kwargs)
-        initial = kwargs.get('initial', {})
-        instance_id = initial.get('instance_id')
-        self.fields['instance_id'] = forms.CharField(widget=forms.HiddenInput,
-                                                     initial=instance_id)
-        class_name = initial.get('class_name')
-        params_of_the_class = self.populate_params_of_the_class(class_name)
-        for key, value in params_of_the_class.items():
-            self.fields[key] = forms.CharField(label=key)
-            self.fields[key].required = True
-            self.fields[key].help_text = key
-            self.fields[key].initial = value
+        # initial = kwargs.get('initial', {})
+        # instance_id = initial.get('instance_id')
+        # self.fields['instance_id'] = forms.CharField(widget=forms.HiddenInput,
+        #                                              initial=instance_id)
+        # class_name = initial.get('class_name')
+        # params_of_the_class = self.populate_params_of_the_class(class_name)
+        # for key, value in params_of_the_class.items():
+        #     self.fields[key] = forms.CharField(label=key)
+        #     self.fields[key].required = True
+        #     self.fields[key].help_text = key
+        #     self.fields[key].initial = value
 
     def handle(self, request, data):
         try:
