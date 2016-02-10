b00e582c3790d140fef6a8d3386968892a245ccb
Iryna Dobenka <Iryna.Anatoliyivna@gmail.com>
2016-02-10 11:21:38 -0500

    code clean up

--- openstack_dashboard/dashboards/advanced/mrpuppet/utils.py
+++ openstack_dashboard/dashboards/advanced/mrpuppet/utils.py
@@ -11,12 +11,11 @@ def set_metadata(request, instance_id, metadata):
 	api.nova.server_metadata_update(request, instance_id, metadata)
 
 def get_enc_metadata(request, instance_id):
-	server = api.nova.server_get(request, instance_id).to_dict()
-	metadata = server['metadata']
+	metadata = get_metadata(request, instance_id)
 	enc_values = [yaml.load(enc_value) for (class_name, enc_value) in metadata.items() if "enc" in class_name]
 	return {enc_value.keys()[0]: enc_value.values()[0] for enc_value in enc_values}
 
 def set_enc_metadata(request, instance_id, class_name, parameters):
     enc_metadata = {class_name: parameters}
     metadata = {"enc_"+class_name: "---\n"+yaml.safe_dump(enc_metadata, allow_unicode=None)}
-    set_metadata_as_dict(request, instance_id, metadata)
+    set_metadata(request, instance_id, metadata)
