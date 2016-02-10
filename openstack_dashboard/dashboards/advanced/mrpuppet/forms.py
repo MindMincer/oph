58bee764930ab385aac3e491dd5295c6af89d239
Iryna Dobenka <Iryna.Anatoliyivna@gmail.com>
2016-02-10 07:18:23 -0500

    disallow edit servers enc

--- openstack_dashboard/dashboards/advanced/mrpuppet/forms.py
+++ openstack_dashboard/dashboards/advanced/mrpuppet/forms.py
@@ -177,31 +177,31 @@ class AddENCMetadata(forms.SelfHandlingForm):
                                                         'data-classessource-' + the_class: param}
 
     def get_current_classes(self, instance_id):
+        # server = api.nova.server_get(self.request, instance_id).to_dict()
+        # metadatas = server['metadata']
+        # api.nova.server_metadata_delete(self.request, instance_id, metadatas.keys())
+
+        # server = api.nova.server_get(self.request, PUPPET_SERVER_ID).to_dict()
+        # metadatas = server['metadata']
+        # api.nova.server_metadata_delete(self.request, PUPPET_SERVER_ID, metadatas.keys())
+
+        # metadatas = {
+        #             "enc_java_env":"---\n"+yaml.safe_dump({"java_env":{"version":"8.0", "security_level":"high"}}, allow_unicode=None),
+        #             "enc_python_env":"---\n"+yaml.safe_dump({"python_env":{"version":"8.0", "security_level":"high"}}, allow_unicode=None),
+        #             "enc_virtualbox_Linux":"---\n"+yaml.safe_dump({"virtualbox_Linux":{"os":"Linux", "version":"2.4", "usb_driver":"true", "storage":"100GB"}}, allow_unicode=None),
+        #             "enc_Windows":"---\n"+yaml.safe_dump({"virtualbox_Windows":{"os":"Windows", "version":"7.0", "usb_driver":"false", "cpu":"2"}}, allow_unicode=None),
+        #             "enc_FreeBSD":"---\n"+yaml.safe_dump({"virtualbox_FreeBSD":{"os":"FreeBSD", "version":"1.3", "media":"true"}}, allow_unicode=None)
+        #             }
+        # api.nova.server_metadata_update(self.request, PUPPET_SERVER_ID, metadatas)
+
+        # api.nova.server_metadata_update(self.request, instance_id, {"clusters":"10"})
+
+
+
+
         server = api.nova.server_get(self.request, instance_id).to_dict()
         metadatas = server['metadata']
-        api.nova.server_metadata_delete(self.request, instance_id, metadatas.keys())
-
-        server = api.nova.server_get(self.request, PUPPET_SERVER_ID).to_dict()
-        metadatas = server['metadata']
-        api.nova.server_metadata_delete(self.request, PUPPET_SERVER_ID, metadatas.keys())
-
-        metadatas = {
-                    "enc_java_env":"---\n"+yaml.safe_dump({"java_env":{"version":"8.0", "security_level":"high"}}, allow_unicode=None),
-                    "enc_python_env":"---\n"+yaml.safe_dump({"python_env":{"version":"8.0", "security_level":"high"}}, allow_unicode=None),
-                    "enc_virtualbox_Linux":"---\n"+yaml.safe_dump({"virtualbox_Linux":{"os":"Linux", "version":"2.4", "usb_driver":"true", "storage":"100GB"}}, allow_unicode=None),
-                    "enc_Windows":"---\n"+yaml.safe_dump({"virtualbox_Windows":{"os":"Windows", "version":"7.0", "usb_driver":"false", "cpu":"2"}}, allow_unicode=None),
-                    "enc_FreeBSD":"---\n"+yaml.safe_dump({"virtualbox_FreeBSD":{"os":"FreeBSD", "version":"1.3", "media":"true"}}, allow_unicode=None)
-                    }
-        api.nova.server_metadata_update(self.request, PUPPET_SERVER_ID, metadatas)
-
-        api.nova.server_metadata_update(self.request, instance_id, {"clusters":"10"})
-
-
-
-
-        server = api.nova.server_get(self.request, instance_id).to_dict()
-        metadatas = server['metadata']
-        enc_metadatas = {"classes":{enc_value.keys()[0]:enc_value.values()[0] for (class_name, yaml.load(enc_value)) in metadatas.items() if "enc" in class_name}}
+        enc_metadatas = {"classes":{yaml.load(enc_value).keys()[0]:yaml.load(enc_value).values()[0] for (class_name, enc_value) in metadatas.items() if "enc" in class_name}}
         if enc_metadatas['classes'].items():
             return enc_metadatas['classes'].keys()
         else:
@@ -216,7 +216,7 @@ class AddENCMetadata(forms.SelfHandlingForm):
     def populate_args_choices(self):
         server = api.nova.server_get(self.request, PUPPET_SERVER_ID).to_dict()
         metadatas = server['metadata']
-        enc_metadatas = {"classes":{enc_value.keys()[0]:enc_value.values()[0] for (class_name, yaml.load(enc_value)) in metadatas.items() if "enc" in class_name}}
+        enc_metadatas = {"classes":{yaml.load(enc_value).keys()[0]:yaml.load(enc_value).values()[0] for (class_name, enc_value) in metadatas.items() if "enc" in class_name}}
         return enc_metadatas['classes']
 
     def handle(self, request, data):
